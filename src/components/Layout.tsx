16fd36660e38e15edf4622dba59120f0c4ed2190
ygkn <2000ygkn0713@gmail.com>
2020-12-13 14:03:19 +0900

    add logout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,17 +4,28 @@ import {
   Container,
   Flex,
   Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Spinner,
 } from '@chakra-ui/react';
 import firebase from 'firebase';
 import Router from 'next/router';
 import { FC } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { FaChevronDown } from 'react-icons/fa';
 
 const login = async (): Promise<void> => {
   await firebase
     .auth()
     .signInWithPopup(new firebase.auth.TwitterAuthProvider());
+
+  Router.push('/');
+};
+
+const logout = async (): Promise<void> => {
+  await firebase.auth().signOut();
 
   Router.push('/');
 };
@@ -36,7 +47,20 @@ export const Layout: FC = ({ children }) => {
             </Button>
           )}
           {!loading && user && (
-            <Avatar name={user?.displayName} src={user?.photoURL} />
+            <Menu>
+              <MenuButton>
+                <Button rightIcon={<FaChevronDown />} variant="ghost">
+                  <Avatar
+                    name={user?.displayName}
+                    src={user?.photoURL}
+                    size="sm"
+                  />
+                </Button>
+              </MenuButton>
+              <MenuList color="gray.800">
+                <MenuItem onClick={logout}>ログアウト</MenuItem>
+              </MenuList>
+            </Menu>
           )}
         </Flex>
 
