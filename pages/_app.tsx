9258d7444f4fe5dd0ee57fbdb91d90a47eac47ca
ygkn <2000ygkn0713@gmail.com>
2020-12-13 12:29:59 +0900

    setup firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import firebase from 'firebase/app';
 import type { AppProps } from 'next/app';
 import { VFC } from 'react';
+
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAvUkEUbLzjZlmlXOs0aDYZipsCAi_ZKZI',
+  authDomain: 'powerpokaraoke.firebaseapp.com',
+  projectId: 'powerpokaraoke',
+  storageBucket: 'powerpokaraoke.appspot.com',
+  messagingSenderId: '659447751310',
+  appId: '1:659447751310:web:54908d1b7ead0bb43b9fdf',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
   return (
