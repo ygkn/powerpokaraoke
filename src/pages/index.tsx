d4ef04ec5e748d688c36d505084581a44610ee43
ygkn <2000ygkn0713@gmail.com>
2020-12-20 16:36:57 +0900

    remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { VFC } from 'react';
 
 import { Layout } from '../components/Layout';
-import { NewRoom } from '../components/NewRoom';
 import { PrivatePage } from '../components/PrivatePage';
 import { RoomList } from '../components/RoomList';
 
